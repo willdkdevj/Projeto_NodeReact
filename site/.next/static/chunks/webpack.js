0f5e3fba6dd980442b8359816dcdd636c1bcba9f
William Derek Dias <williamdkdevops@gmail.com>
2021-03-04 01:21:48 -0300

    Editado README adicionando as etapas do desenvolvimento do site

--- site/.next/static/chunks/webpack.js
+++ site/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ee979a082708b593fe45";
+/******/ 	var hotCurrentHash = "9021b6800d684361d0c6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
